dfb9daea20116bb2b73d6ea717eb281df9ef24bd
Aaron Larner <anlarner@gmail.com>
2016-06-28 21:04:55 -0500

    Create models

--- models/User.js
+++ models/User.js
@@ -1,8 +1,16 @@
 require('./Authentication');
+require('./Link');
+require('./WorkHistory');
 module.exports = bookshelf.model('User', {
 	tableName: 'users',
 	hasTimestamps: ['createdAt', 'updatedAt', 'deletedAt'],
 	authentication: function() {
 		return this.hasMany('Authentication', 'userId');
+	},
+	links: function() {
+		return this.hasMany('Link', 'userId');
+	},
+	workHistory: function() {
+		return this.hasMany('WorkHistory', 'userId');
 	}
 });
